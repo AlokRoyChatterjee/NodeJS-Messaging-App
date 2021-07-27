c6dd58fa9bca437204692e4a845dc8dfbd9a84eb
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-27 12:11:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.get('/', function(req, res) {
 io.sockets.on('connection', function(socket) {
     socket.on('username', function(username) {
         socket.username = username;
-        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+        io.emit('is_online', '<i>' + socket.username + ' join the chat..</i>');
     });
 
     socket.on('disconnect', function(username) {
-        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+        io.emit('is_online', '<i>' + socket.username + ' left the chat..</i>');
     })
 
     socket.on('chat_message', function(message) {
@@ -24,5 +24,5 @@ io.sockets.on('connection', function(socket) {
 });
 
 const server = http.listen(8080, function() {
-    console.log('listening on *:8080');
+    console.log('This messaging app is on 8080');
 });
